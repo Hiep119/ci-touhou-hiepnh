9bc5133af017c03004bd153c4ca1922174d99755
Hiep <hiephoanghiep119@gmail.com>
2017-10-11 20:46:07 +0700

    Added players, background drawing Added size fixing, window closing handling

--- src/GameCanvas.java
+++ src/GameCanvas.java
@@ -1,8 +1,34 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class GameCanvas extends JPanel {
 
+    BufferedImage background; // copy ảnh từ ổ cứng vào ram (biến)
+    BufferedImage players;
+
     public GameCanvas() {
+
+        //1. Load background
+        try { // thử xem link lỗi ko?
+            background = ImageIO.read(new File("assets/images/background/0.png"));
+            players = ImageIO.read(new File("assets/images/players/straight/5.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    //2. Draw background
+
+    @Override
+    protected void paintComponent(Graphics g) { // vẽ component >> component con
+        g.drawImage(background, 0, 0, null);
+        g.drawImage(players, 175, 520, null);
 
     }
 }
--- src/GameWindow.java
+++ src/GameWindow.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
 public class GameWindow extends JFrame{
 
@@ -7,12 +9,57 @@ public class GameWindow extends JFrame{
 
     public GameWindow() {
         this.setSize(800, 600);
-        this.setVisible(true);
-        this.setBackground(Color.black);
-
 
         this.canvas = new GameCanvas();
         this.setContentPane(this.canvas); // cho canvas vào khung
+
+        this.addWindowListener(new WindowListener() {
+            @Override
+            public void windowOpened(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowClosing(WindowEvent e) {
+               System.exit(0); // thoát >> giải phóng bộ nhớ.
+            }
+
+            @Override
+            public void windowClosed(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowIconified(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowDeiconified(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowActivated(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowDeactivated(WindowEvent e) {
+
+            }
+        }); // cách 1
+        //this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);    // cách 2: tắt màn (nếu không có vẫn chạy ngầm)
+
+        this.setResizable(false); // không cho resize màn hình lúc di chuyển
+
+        this.setBackground(Color.blue);
+        this.canvas.setVisible(true);
+        this.setVisible(true);  // setVisible con trước
+
+
+        //this.canvas.repaint(); // báo cho canvas vẽ lại (khi ko thấy canvas vẽ)
+
     }
 
 
